727713d093da7ba3950753381afdeb0873e703c6
clpiano <111512024+clpiano@users.noreply.github.com>
2022-09-11 15:27:33 +0000

    [save]kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,37 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all #@bookから@booksへ、newからallへ変更
+    @books = Book.all
   end
   
-  def create
+  def create #保存アクション
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id) #詳細画面へリダイレクト
   end  
   
   def show
     @book = Book.find(params[:id])
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books' #投稿一覧画面へ
+  end  
+  
   def edit
     @book = Book.find(params[:id])
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   private
   
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
